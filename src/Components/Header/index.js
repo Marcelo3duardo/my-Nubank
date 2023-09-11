4d46dde32b165e0d3e040aa44f39f1ddf3236f70
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2023-09-11 15:02:58 -0300

    Add: save name User

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,42 @@
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet, Text, TouchableOpacity, TextInput, StatusBar, Pressable } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const heightStatusBar = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64
+const heightStatusBar = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 64;
 
 export default function Header() {
-    const [userName, setUserName] = useState('User Name')
+    const [userName, setUserName] = useState('User Name');
+
+    useEffect(() => {
+        async function getUserName() {
+            try {
+                const aux = await AsyncStorage.getItem('@my-Nubank:UserName');
+                if (aux !== null) {
+                    setUserName(aux);
+                    console.log(aux);
+                }
+            } catch (error) {
+                console.error("Error getting user name:", error);
+            }
+        }
+        
+        getUserName();
+    }, []);
+
+    async function saveName(name) {
+        try {
+            await AsyncStorage.setItem('@my-Nubank:UserName', name);
+        } catch (error) {
+            console.error("Error saving user name:", error);
+        }
+    }
+
+    const handleUserNameChange = (newName) => {
+        setUserName(newName);
+        saveName(newName);
+    }
 
     return (
         <View style={styles.container}>
@@ -14,24 +44,21 @@ export default function Header() {
                 <View style={styles.containerName}>
                     <TextInput
                         style={styles.userName}
-                        placeholder="Adicione Name"
-                        onChangeText={userName => setUserName(userName)}
-                        defaultValue={userName}
+                        placeholder="Adicione Nome"
+                        onChangeText={handleUserNameChange}
+                        value={userName}
                         maxLength={25}
                     />
-
                     <AntDesign name="edit" size={24} color="white" />
                 </View>
-
                 <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
-
                     <AntDesign name="user" size={27} style={styles.iconUser} color="#157" />
                     {/* <Text style={styles.textUser}> Por Enquanto</Text> */}
                 </TouchableOpacity>
             </View>
-        </View>)
+        </View>
+    );
 }
-
 const styles = StyleSheet.create({
     container: {
         paddingTop: heightStatusBar,
@@ -46,7 +73,7 @@ const styles = StyleSheet.create({
 
 
     },
-    containerName:{
+    containerName: {
         display: "flex",
         flexDirection: "row"
     },
